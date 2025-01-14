bcbd97a6372abedb7a04ffb9e558643c46a7eca8
The4ing <atar.orram101@gmail.com>
2025-01-14 19:44:36 +0200

    Update GameManager.cpp

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -2,75 +2,51 @@
 #include <iostream>
 #include <fstream>
 #include <SFML/Audio.hpp>
+#include "Board.h"
 
 GameManager::GameManager()
     : m_window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "BOMBERMAN"),
-    m_mainMenu(&m_window, 0),
-    m_gameScreen(&m_window),
-    m_settingsScreen(&m_window),
-    m_board()
+    m_mainMenu(&m_window, 0),             // Initialize MainMenuDisplay with window and dummy game ID
+    m_gameScreen(&m_window),              // Initialize GameDisplay with window
+    m_settingsScreen(&m_window)           // Initialize SettingsDisplay with window
+
 {
+    m_board.loadFromFile("level1.txt");
+    m_board.loadTextures();
+}
 
-
-    //for timer
-    if (!m_font.loadFromFile("PixelFontBlack.otf")) {
-        std::cerr << "Error loading font!" << std::endl;
-    }
-    m_timerText.setFont(m_font);
-    m_timerText.setCharacterSize(24);  // Adjust size as needed
-    m_timerText.setFillColor(sf::Color::Red);
-    m_timerText.setPosition(10, 10);  // Set position on the screen
+void GameManager::newGame()
+{
+    m_window.create(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "BOMBERMAN", sf::Style::Titlebar | sf::Style::Close | sf::Style::Resize);
+    m_mainMenu.Run();
+    startGame();
 }
 
 
 void GameManager::startGame() {
-    m_window.create(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "BOMBERMAN", sf::Style::Titlebar | sf::Style::Close);
-
-    //for timer
-    m_board.setLevelDuration(300.0f);  // Set level duration to 5 minutes, for example
-
-
     while (m_window.isOpen()) {
-
-        //for timer
-          m_board.UpdateTimer();  // Update the board timer
-         // Update the timer text
-         m_timerText.setString("Time: " + m_board.getTimeString());
-
-
-         m_mainMenu.Run();
-         // Update game logic
-       
         sf::Event event;
         while (m_window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 m_window.close();
+
+            if (event.type == sf::Event::Resized) {
+                // Adjust the view when the window is resized
+                sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+                m_window.setView(sf::View(visibleArea));
+            }
+            if (event.key.code == sf::Keyboard::Escape) {
+                m_mainMenu.Run();
+            }
         }
 
-        
-
-        // Render everything
-         render();
-        
+        m_window.clear();
+        m_board.display(m_window);
+        m_window.display();
     }
 }
 
 
-
-
 bool GameManager::loadLevel(const std::string& fileName) {
     return true;
-}
-
-
-//add
-void GameManager::render() {
-    m_window.clear();
-
-    // Draw timer
-    m_window.draw(m_timerText);
-
-    // Add other rendering logic here (e.g., game objects)
-
-    m_window.display();
 }
