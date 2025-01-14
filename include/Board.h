cfc8884ac1e96ec96b994ba2f6e63d733497f4a3
The4ing <atar.orram101@gmail.com>
2025-01-14 19:42:43 +0200

    Update Board.h

--- include/Board.h
+++ include/Board.h
@@ -1,9 +1,14 @@
-#include <iostream>
+#pragma once
 #include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <SFML/Graphics.hpp>
 #include <SFML/System/Clock.hpp>  // Include SFML clock
 #include <sstream>
-
-
+#include "Cell.h"
+#include "Wall.h"
+#include "Empty.h"
 
 // Enum to define power-up choices
 enum object {
@@ -22,13 +27,17 @@ public:
     void GrantExtraLife();
     //void RemoveGuard();
 
-
     //this is for the timer 
     void IncreaseTime(const int extraTime);
     std::string getTimeString() const;
     void setLevelDuration(float duration);
 
     // Other members and variables for game state (e.g., guards, lives, timer, etc.)
+    void loadFromFile(const std::string& fileName);
+    void loadTextures();
+    void displayConsole() const;
+    void display(sf::RenderWindow& window) const;
+
 private:
     bool m_FreezeGuardsStatus;    // Tracks whether the guards are frozen
     int m_lives;                  // Number of lives
@@ -39,6 +48,11 @@ private:
     //this is for the timer 
     sf::Clock m_clock;  // Starts the clock immediately
     bool m_isTimerRunning;
+
+    std::vector<std::vector<Cell>> grid;
+    int m_rows, m_cols;
+
+    sf::Texture wallTexture;
+    sf::Texture emptyTexture;
+    sf::View m_view;
 };
-
-
