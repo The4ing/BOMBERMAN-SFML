00ce8ffae269303287c41c08255f0dd854e25d9a
The4ing <atar.orram101@gmail.com>
2025-01-14 19:47:30 +0200

    Add files via upload

---
+++ src/Empty.cpp
@@ -0,0 +1,3 @@
+// empty.cpp
+#include "Empty.h"
+// No additional functionality for now, so this file is empty.
---
+++ src/Wall.cpp
@@ -0,0 +1,3 @@
+// wall.cpp
+#include "Wall.h"
+// No additional functionality for now, so this file is empty.
