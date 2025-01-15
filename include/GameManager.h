9ef847b94e8debcc96b530ef7d4b7e90f8e86905
The4ing <atar.orram101@gmail.com>
2025-01-15 18:38:01 +0200

    Update GameManager.h

--- include/GameManager.h
+++ include/GameManager.h
@@ -5,7 +5,14 @@
 #include "SettingsDisplay.h"
 #include "Board.h"
 
-
+enum Direction {
+    INIT
+    , UP
+    , DOWN
+    , RIGHT
+    , LEFT
+    , STAND
+};
 
 class GameManager {
 public:
@@ -13,16 +20,20 @@ public:
     void newGame();
     void startGame();
     bool loadLevel(const std::string& fileName);
-
+    void processGameEvents();
+    void handleMoveKeys(sf::Keyboard::Key key, const bool isPressed);
+    
 private:
     sf::RenderWindow m_window;   // SFML render window
     MainMenuDisplay m_mainMenu;
     GameDisplay m_gameScreen;
     SettingsDisplay m_settingsScreen;
     Board m_board;
-
+    int m_robotLives;
     //for timer
     sf::Font m_font;          // Font for displaying text
     sf::Text m_timerText;     // Text object for the timer
-
+    bool m_pause;
+    bool m_arrowKeyPressed;
+    Direction m_robotDirection;
 };
