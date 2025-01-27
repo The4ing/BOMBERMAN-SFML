6b819cde850f2d4632dfbdec49afc4bb0cd10486
The4ing <atar.orram101@gmail.com>
2025-01-27 16:06:07 +0200

    Update StupidGuard.h

--- include/StupidGuard.h
+++ include/StupidGuard.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "Guard.h"
+#include <SFML/Graphics.hpp>
 #include <random>
-
 
 class StupidGuard : public Guard {
 public:
@@ -12,22 +12,27 @@ public:
     char getSymbol() const override { return '!'; }
     void setPosition(float x, float y) override;
     sf::Vector2f getPosition() const override;
-    void update(const float deltaTime) override;
+    void update(float deltaTime) override;
     void draw(sf::RenderWindow& window) const override;
+    void handleCollision(GameObject& other) override;
+    virtual sf::FloatRect getBoundingBox() const override;
 
-  
-   
-
-
+    // Specific collision handling
+    void handleCollisionWith(Robot& robot) override;
+    void handleCollisionWith(Wall& wall) override;
+    void handleCollisionWith(Rock& rock) override;
+    void handleCollisionWith(Door& door) override;
+    void handleCollisionWith(Guard& guard) override;
+    void handleCollisionWith(Bomb&, bool isExploding) override;        // No-op for Bomb
+    void setScale(float scaleX, float scaleY) override;
+    sf::CircleShape getCollisionShape() const override;
 private:
     // Helper Methods
     void changeDirection();
-
+    void revertPosition();
     // Animation-related members
-    //sf::Texture m_texture;          // Texture for the guard's sprite
-    //sf::Sprite m_sprite;            // Sprite for the guard
-
-
+    sf::Texture m_texture;          // Texture for the guard's sprite
+    sf::Sprite m_sprite;            // Sprite for the guard
     int m_frameWidth;               // Width of a single frame
     int m_frameHeight;              // Height of a single frame
     int m_animationFrame;           // frame of the animation
@@ -37,7 +42,10 @@ private:
     sf::Time m_timeSinceLastChange;
     sf::Time m_animationTimeSinceLastChange;
 
+    sf::CircleShape m_collisionShape;
+
     // Movement-related members
     sf::Vector2f m_velocity;        // Guard's current velocity
+    sf::Vector2f m_previousPosition;        // Guard's current velocity
     Direction m_direction;          // Current movement direction
 };
