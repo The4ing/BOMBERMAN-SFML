42cfe7a871ae6691ab048e327ba9ff588d6b64e2
The4ing <atar.orram101@gmail.com>
2025-01-27 16:05:04 +0200

    Update GameObject.h

--- include/GameObject.h
+++ include/GameObject.h
@@ -1,36 +1,31 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "Utilities.h"
-#include <iostream>
-
-class Robot;  // Forward declaration
+class Robot;
 class Wall;
-class Guard;  // Forward declaration
-class Door;
 class Rock;
+class Door;
+class Guard;
 class Bomb;
 
 class GameObject {
 public:
     virtual ~GameObject() = default;
 
-    virtual void collideWith(GameObject* other) = 0;
-    virtual void collideWith(Robot* robot) = 0;
-    virtual void collideWith(Guard* guard) = 0;
-    virtual void collideWith(Bomb* bomb) = 0;
-
-
-
-    virtual sf::FloatRect getGlobalBounds() const {
-        return m_sprite.getGlobalBounds();
-    }
-
     virtual char getSymbol() const = 0;
     virtual sf::Vector2f getPosition() const = 0;
     virtual void setPosition(float newX, float newY) = 0;
     virtual void draw(sf::RenderWindow& window) const = 0;
+    virtual sf::FloatRect getBoundingBox() const = 0;
 
-protected:
-    sf::Sprite m_sprite;
-    sf::Texture m_texture;
+    // Entry point for double dispatch
+    virtual void handleCollision(GameObject& other) = 0;
+
+    // Overloads for specific collision types
+    virtual void handleCollisionWith(Robot&) = 0;
+    virtual void handleCollisionWith(Wall&) = 0;
+    virtual void handleCollisionWith(Rock&) = 0;
+    virtual void handleCollisionWith(Door&) = 0;
+    virtual void handleCollisionWith(Guard&) = 0;
+    virtual void handleCollisionWith(Bomb&, bool isExploding) = 0; 
 };
