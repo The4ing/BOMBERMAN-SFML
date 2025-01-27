db31ea8a1e12115b4a08feb648c3fce778db0669
The4ing <atar.orram101@gmail.com>
2025-01-27 16:05:19 +0200

    Update Guard.h

--- include/Guard.h
+++ include/Guard.h
@@ -1,26 +1,21 @@
 #pragma once
-#include <iostream>
-#include "MovingGameObject.h"
 
+#include "MovingGameObject.h"
+const int CIRCLRE_OFFSET = 10;
 class Guard : public MovingGameObject {
 protected:
-
-   
     // Shared attributes and behavior for all guards
     int guardID;
     float detectionRange;
-  
 
 public:
     Guard() : guardID(0), detectionRange(100.0f) {}
-   
-    void collideWith(GameObject* other) override;
-    void collideWith(Rock* rock) override;
-    void collideWith(Wall* wall) override;
-    void collideWith(Door* door) override;
-    void collideWith(Guard* Guard) override;
-    void collideWith(Robot* robot) override;
-    void collideWith(Bomb* bomb) override;
+  //  virtual void colision() = 0; // Pure virtual function
+    virtual void setScale(float scaleX, float scaleY) = 0;
+  //  virtual void reverseDirection() = 0; 
+    //    m_direction.x = -m_direction.x; // Reverse X direction
+    //    m_direction.y = -m_direction.y; // Reverse Y direction
+    //}
 
 };
 
