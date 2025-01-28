d6121b00d0d8c304da67cf60d55011081f8df8cd
The4ing <atar.orram101@gmail.com>
2025-01-28 11:44:29 +0200

    Update Guard.cpp

--- src/Guard.cpp
+++ src/Guard.cpp
@@ -1,42 +1,41 @@
-#include "Guard.h"
-
-
-
-
-void Guard::collideWith(GameObject* other)
-{
-    other->collideWith(this);  // Double dispatch
-
-}
-
-void Guard::collideWith(Rock* rock)
-{
-    std::cout << "Guard collided with a Rock!" << std::endl;
-
-}
-
-void Guard::collideWith(Wall* wall)
-{
-    std::cout << "SmGuardartGuard collided with a Wall!" << std::endl;
-}
-
-void Guard::collideWith(Door* Door)
-{
-    std::cout << "Guard collided with a Door!" << std::endl;
-}
-
-void Guard::collideWith(Guard* Guard)
-{
-	std::cout << "Guard collided with a Guard!" << std::endl;
-}
-
-void Guard::collideWith(Robot* robot) {
-    std::cout << "Guard collided with a Robot!" << std::endl;
-    // Handle collision with robot (e.g., take damage, stop movement, etc.)
-}
-
-void Guard::collideWith(Bomb* bomb)
-{
-    std::cout << "Guard collided with a Bomb!" << std::endl;
-}
-
+//#include "Guard.h"
+//
+//
+//
+//
+//void Guard::collideWith(GameObject* other)
+//{
+//    other->collideWith(this);  // Double dispatch
+//
+//}
+//
+//void Guard::collideWith(Rock* rock)
+//{
+//    std::cout << "Guard collided with a Rock!" << std::endl;
+//
+//}
+//
+//void Guard::collideWith(Wall* wall)
+//{
+//    std::cout << "SmGuardartGuard collided with a Wall!" << std::endl;
+//}
+//
+//void Guard::collideWith(Door* Door)
+//{
+//    std::cout << "Guard collided with a Door!" << std::endl;
+//}
+//
+//void Guard::collideWith(Guard* Guard)
+//{
+//    std::cout << "Guard collided with a Guard!" << std::endl;
+//}
+//
+//void Guard::collideWith(Robot* robot) {
+//    std::cout << "Guard collided with a Robot!" << std::endl;
+//    // Handle collision with robot (e.g., take damage, stop movement, etc.)
+//}
+//
+//void Guard::collideWith(Bomb* bomb)
+//{
+//    std::cout << "Guard collided with a Bomb!" << std::endl;
+//}
