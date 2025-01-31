05e9c93ed653718c8d967bad9cb775a535bd3e5f
Romi sinizkey <sinizromi@gmail.com>
2025-02-01 00:28:12 +0200

    Update Wall.cpp

--- src/Wall.cpp
+++ src/Wall.cpp
@@ -1,8 +1,9 @@
 #include "Wall.h"
 #include <iostream>
 
-Wall::Wall(const sf::Texture& texture) {
-    m_sprite.setTexture(texture);
+Wall::Wall() {
+    ResourceManager& resourceManager = ResourceManager::getInstance();
+    m_sprite.setTexture(resourceManager.getTexture("wall.png"));
 }
 
 char Wall::getSymbol() const {
@@ -27,28 +28,32 @@ void Wall::handleCollision(GameObject& other) {
 }
 
 void Wall::handleCollisionWith(Robot& robot) {
-    std::cout << "Wall collided with Robot\n";
-    // No additional behavior, but logic can be extended if needed.
+    // std::cout << "Wall collided with Robot\n";
+     // No additional behavior, but logic can be extended if needed.
 }
 
 void Wall::handleCollisionWith(Wall& wall) {
-    std::cout << "Wall collided with another Wall\n";
-    // Walls generally don't react to other walls.
+    // std::cout << "Wall collided with another Wall\n";
+     // Walls generally don't react to other walls.
 }
 
 void Wall::handleCollisionWith(Rock& rock) {
-    std::cout << "Wall collided with Rock\n";
+    //std::cout << "Wall collided with Rock\n";
     // No behavior defined for Wall-Rock collision.
 }
 
 void Wall::handleCollisionWith(Door& door) {
-    std::cout << "Wall collided with Door\n";
+    //std::cout << "Wall collided with Door\n";
     // No behavior defined for Wall-Door collision.
 }
 
 void Wall::handleCollisionWith(Guard& guard) {
-    std::cout << "Wall collided with Guard\n";
+    //std::cout << "Wall collided with Guard\n";
     // No behavior defined for Wall-Guard collision.
+}
+void Wall::handleCollisionWith(Present& Present)
+{
+    //do nothing 
 }
 void Wall::handleCollisionWith(Bomb&, bool isExploding) {
     // No-op: Rocks don't react to Guards
