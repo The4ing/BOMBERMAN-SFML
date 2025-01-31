ba31a3feca4b1bce8bc583cb44657c02ea8af784
Romi sinizkey <sinizromi@gmail.com>
2025-02-01 00:26:42 +0200

    Update ResourceManager.cpp

--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -1,25 +1,81 @@
 #include "ResourceManager.h"
 
+
+ResourceManager::ResourceManager() {
+    loadTexture();
+    loadFont();
+}
+
 ResourceManager& ResourceManager::getInstance() {
+    
     static ResourceManager instance;
     return instance;
 }
 
+void ResourceManager::loadTexture() {
 
-bool ResourceManager::loadTexture(const std::string& filename) {
-    return m_textures[filename].loadFromFile(filename );
+    std::vector<std::string> m_NamefilesTextures =
+    {
+        {"wall.png"},
+         {"rock.png"},
+        { "guard.png"},
+        {"door.png"},
+        {"empty.png"},
+        {"bomb.png"},
+        {"present.png"},
+        { "arrow.png"},
+        {"bombXPLD.png"},
+        {"clock.png"},
+        {"explosion.png"},
+        {"heart.png"},
+        {"helpBackground.png"},
+        {"menu.png"},
+        {"mute.png"},
+        {"Robot.png"},
+        {"RobotXPL.png"},
+        {"robot_spritesheet.png"},
+        {"Rock.png"},
+        {"scary_guard_spritesheet.png"},
+        {"smartGuardSprite.png"},
+        {"timer.png"},
+        {"unmute.png"},
+        {"wall.png"},
+
+    };
+    for (const auto& filename : m_NamefilesTextures) {
+        if (!m_textures[filename].loadFromFile(filename)) {
+            std::cerr << "Error: Could not load texture file " << filename << std::endl;
+        }
+    }
+    
 }
 
-bool ResourceManager::loadFont(const std::string& filename) {
-    return m_font.loadFromFile(filename);
+void ResourceManager::loadFont() {
+    std::vector<std::string> fontFiles = {
+        "Ariel.ttf",
+        "digit.ttf",  // Fixed typo
+        "PixelFontBlack.otf",
+        "PixelFontWhite.otf"
+    };
+
+    for (const auto& filename : fontFiles) {
+        sf::Font font;
+        if (!font.loadFromFile(filename)) {
+            std::cerr << "Error: Could not load font file " << filename << std::endl;
+        }
+        else {
+            m_font[filename] = std::move(font);  // Store the successfully loaded font
+        }
+    }
 }
+
 
 const sf::Texture& ResourceManager::getTexture(const std::string& key) const {
     return m_textures.at(key);
 }
 
-const sf::Font& ResourceManager::getFont() const {
-    return m_font;
+const sf::Font& ResourceManager::getFont(const std::string& key) const {
+    return m_font.at(key);
 }
 
 void ResourceManager::addSprite(const std::string& key, const sf::Sprite& sprite) {
