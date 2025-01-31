0377ed0fad1a129b2a48353eb69533b6653b518a
Romi sinizkey <sinizromi@gmail.com>
2025-02-01 00:15:33 +0200

    Create FreezeGuard.h

---
+++ include/FreezeGuard.h
@@ -0,0 +1,17 @@
+#include "Present.h"
+
+
+class FreezeGuard : public Present {
+
+public:
+	FreezeGuard(); // Pass by reference
+
+
+	char getSymbol() const override; // Unique symbol
+
+	
+
+private:
+
+
+};
