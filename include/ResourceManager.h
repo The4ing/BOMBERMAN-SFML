03a419cf2d98ca771f621d312d37236ca07ff79e
Romi sinizkey <sinizromi@gmail.com>
2025-01-31 20:54:14 +0200

    Update ResourceManager.h

--- include/ResourceManager.h
+++ include/ResourceManager.h
@@ -4,6 +4,23 @@
 #include <unordered_map>
 #include <iostream>
 
+
+const float TOOLBAR_HEIGHT = 100.0f; // Fixed toolbar height in pixels
+const float WINDOW_WIDTH = 1920.0f; // Example window width
+const float WINDOW_HEIGHT = 1080.0f; // Example window height
+const float SINGLE_SPRITE_DIMENSIONS = 500.0f; 
+
+
+enum Direction {
+    INIT
+    , LEFT 
+    , DOWN
+    , UP
+    , RIGHT
+    , STAND
+};
+
+
 class ResourceManager {
 public:
     static ResourceManager& getInstance();
