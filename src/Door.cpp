f862b6a65781c3dd335da58f7ed325010ec34c2a
The4ing <atar.orram101@gmail.com>
2025-02-06 21:25:29 +0200

    Update Door.cpp

--- src/Door.cpp
+++ src/Door.cpp
@@ -3,37 +3,17 @@
 
 Door::Door() {
     ResourceManager& resourceManager = ResourceManager::getInstance();
-    setTexture(resourceManager.getTexture("door.png")); // Assign the texture to the sprite
+    setTexture(resourceManager.getTexture("door.png"));
 }
 
 char Door::getSymbol() const {
-    return 'D'; // Represents the Door in the game
+    return 'D'; 
 }
-
-//sf::Vector2f Door::getPosition() const {
-//    return m_sprite.getPosition(); // Returns the sprite's current position
-//}
-
-//void Door::setPosition(float x, float y) {
-//    m_sprite.setPosition(x, y); // Sets the sprite's position
-//}
-//void Door::setScale(float scaleX, float scaleY) {
-//    m_sprite.setScale(scaleX, scaleY);
-//}
-
-
 void Door::draw(sf::RenderWindow& window) const {
   
     window.draw(getSprite());
 }
 
 void Door::handleCollision(GameObject& other) {
-    other.handleCollisionWith(*this); // Delegate collision handling to the other object
+    other.handleCollisionWith(*this); 
 }
-
-
-//sf::FloatRect Door::getBoundingBox() const {
-//
-//    return m_sprite.getGlobalBounds(); // Returns the bounding box of the door
-//}
-
