caa59f814cbb280b73fd33a9d0d3178c11b83167
The4ing <atar.orram101@gmail.com>
2025-02-06 21:11:09 +0200

    Update Door.h

--- include/Door.h
+++ include/Door.h
@@ -10,21 +10,15 @@ class Guard;
 
 class Door : public GameObject {
 public:
-
-
-    explicit Door(); // Constructor to initialize the door with a texture
-    char getSymbol() const override;  // Returns 'D' to represent a door
-    void draw(sf::RenderWindow& window) const override; // Draws the door on the screen
-
-    void handleCollision(GameObject&) override; // Double dispatch entry point
-    void handleCollisionWith(Robot&) override {} // Handle collision with a Robot
-    void handleCollisionWith(Wall&) override {}       // No-op for Wall
-    void handleCollisionWith(Rock&) override {}        // No-op for Rock
-    void handleCollisionWith(Door&) override {}       // No-op for another Door
-    void handleCollisionWith(Guard&) override {}      // No-op for Guard
+    explicit Door(); 
+    char getSymbol() const override; 
+    void draw(sf::RenderWindow& window) const override; 
+    void handleCollision(GameObject&) override; 
+    void handleCollisionWith(Robot&) override {} 
+    void handleCollisionWith(Wall&) override {}       
+    void handleCollisionWith(Rock&) override {}        
+    void handleCollisionWith(Door&) override {}      
+    void handleCollisionWith(Guard&) override {}      
     void handleCollisionWith(Present& Present) override{}
-    void handleCollisionWith(Bomb&, bool) override {}       // No-op for Bomb
-   
-
-
+    void handleCollisionWith(Bomb&, bool isExploding) override {}
 };
