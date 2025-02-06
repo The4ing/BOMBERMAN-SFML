998c12125271dce6c82b570b623dcbd4984f7e01
The4ing <atar.orram101@gmail.com>
2025-02-06 21:17:59 +0200

    Update Rock.h

--- include/Rock.h
+++ include/Rock.h
@@ -10,21 +10,15 @@ class Guard;
 
 class Rock : public GameObject {
 public:
-    explicit Rock(); // Constructor to initialize the Rock with a texture
-    char getSymbol() const override;  // Returns '#' to represent a Rock
-    void draw(sf::RenderWindow& window) const override; // Draws the Rock on the screen
-
-
-
-    void handleCollision(GameObject& other) override; // Double dispatch entry point
-    void handleCollisionWith(Robot& ) override {}// Handle collision with a Robot
-    void handleCollisionWith(Wall&) override {}        // No-op for Wall
-    void handleCollisionWith(Rock&) override {}      // No-op for another Rock
-    void handleCollisionWith(Door&) override {}        // No-op for Door
-    void handleCollisionWith(Guard&) override {}       // No-op for Guard
+    explicit Rock(); 
+    char getSymbol() const override;  
+    void draw(sf::RenderWindow& window) const override; 
+    void handleCollision(GameObject& other) override; 
+    void handleCollisionWith(Robot& ) override {}
+    void handleCollisionWith(Wall&) override {}      
+    void handleCollisionWith(Rock&) override {}     
+    void handleCollisionWith(Door&) override {}      
+    void handleCollisionWith(Guard&) override {}      
     void handleCollisionWith(Present& ) override {}
-    void handleCollisionWith(Bomb&, bool ) override {}       // No-op for Bomb
-    
-
-
+    void handleCollisionWith(Bomb&, bool isExploding) override {}   
 };
