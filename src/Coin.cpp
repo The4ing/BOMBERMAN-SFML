7281918d3dd473422ab0bc27a043b61c99f43ed4
The4ing <atar.orram101@gmail.com>
2025-02-06 21:25:18 +0200

    Update Coin.cpp

--- src/Coin.cpp
+++ src/Coin.cpp
@@ -5,14 +5,10 @@
 Coin::Coin() : frameTime(0.1f), elapsedTime(0.0f), frameIndex(0), totalFrames(8) {
     ResourceManager& resourceManager = ResourceManager::getInstance();
     setTexture(resourceManager.getTexture("Coin.png"));
-    // Assume each frame is 64x64 pixels (adjust based on your sprite sheet)
-    frameRect = sf::IntRect(0, 0, 17, 17);
+    frameRect = sf::IntRect(0, 0, COIN_SIZE, COIN_SIZE);
     getSprite().setTextureRect(frameRect);
-
-    // Scale the sprite to make it bigger
-    getSprite().setScale(55.0f, 55.0f);  // Double the size of the coin (change values as needed)
+    getSprite().setScale(55.0f, 55.0f); 
 }
-
 
 char Coin::getSymbol() const {
     return '$';
@@ -20,23 +16,14 @@ char Coin::getSymbol() const {
 
 void Coin::draw(sf::RenderWindow& window) const {
     window.draw(getSprite());
-    sf::CircleShape collisionShape = getCollisionShape();
-    collisionShape.setFillColor(sf::Color::Transparent);
-    collisionShape.setOutlineColor(sf::Color::Red);
-    collisionShape.setOutlineThickness(1.f);
-    window.draw(collisionShape);
 }
 
 void Coin::update(float deltaTime) {
     elapsedTime += deltaTime;
-
-    // Change frame if enough time has passed
     if (elapsedTime >= frameTime) {
         elapsedTime = 0.0f;
-        frameIndex = (frameIndex + 1) % totalFrames;  // Cycle through frames
-
-        // Update texture rect to show next frame
-        frameRect.left = frameIndex * 17;  // Move to next frame in the sprite sheet
+        frameIndex = (frameIndex + 1) % totalFrames;  
+        frameRect.left = frameIndex * COIN_SIZE;
         getSprite().setTextureRect(frameRect);
     }
 }
@@ -47,10 +34,9 @@ void Coin::handleCollision(GameObject& other) {
 
 sf::CircleShape Coin::getCollisionShape() const {
     sf::CircleShape collisionCircle;
-    collisionCircle.setRadius(17.0f);
-    // Scale it horizontally to form an ellipse
-    float horizontalScale = 0.7f;  // Adjust this value for width
-    float verticalScale = 1.f;    // Keep this 1.0 to maintain original height
+    collisionCircle.setRadius(static_cast<float>(COIN_SIZE));
+    float horizontalScale = 0.7f;  
+    float verticalScale = 1.f;    
     collisionCircle.setScale(horizontalScale, verticalScale);
 
     collisionCircle.setPosition(getPosition().x + 30 / 2 - 10,
