4eb011ebdd2d6f780d73786f081d006f3274527f
The4ing <atar.orram101@gmail.com>
2025-02-06 21:14:46 +0200

    Update MovingGameObject.h

--- include/MovingGameObject.h
+++ include/MovingGameObject.h
@@ -1,16 +1,11 @@
 #pragma once
 #include "GameObject.h"
-#include <SFML/Graphics/RenderWindow.hpp> // Include only RenderWindow from SFML
-#include <SFML/Window/Keyboard.hpp>       // Include for sf::Keyboard::Key
+#include <SFML/Graphics/RenderWindow.hpp> 
+#include <SFML/Window/Keyboard.hpp>       
 
 class MovingGameObject : public GameObject {
-   // Velocity components (in units per second)
-
+ 
 public:
-  
     virtual sf::CircleShape getCollisionShape() const = 0;
-    virtual void update(float 
-    ) = 0;          
-   
-
+    virtual void update(float) = 0;          
 };
