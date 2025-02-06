caa3bebd552420173193f438b98cba7b6273b5ed
The4ing <atar.orram101@gmail.com>
2025-02-06 04:50:37 +0200

    Update GameManager.cpp

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -15,7 +15,10 @@ GameManager::GameManager()
     m_settingsScreen(&m_window),
     m_currentLevel(1) // Start from Level 1
 {
-    //m_board.loadTextures();
+    ResourceManager& resourceManager = ResourceManager::getInstance();
+    m_muteButtonSprite.setTexture(resourceManager.getTexture("mute.png")); // Default mute icon
+    m_muteButtonSprite.setPosition(WINDOW_WIDTH - 80, 20); // Top-right corner
+
 }
 
 
@@ -25,7 +28,7 @@ void GameManager::newGame() {
     m_pause = false;
 
     std::string levelFile = "level" + std::to_string(m_currentLevel) + ".txt";
-    m_board.loadFromFile(levelFile);
+    m_board.loadFromFile(levelFile, m_currentLevel);
 
     std::cout << "New game started at " << levelFile << "!" << std::endl;
 
@@ -35,13 +38,12 @@ void GameManager::newGame() {
     m_mainMenu.Run();
     startGame();
 }
-
 void GameManager::startGame() {
 
 
 
     ResourceManager& resourceManager = ResourceManager::getInstance();
-    sf::Sprite lostLifeSprite(resourceManager.getTexture("lost_life_screen.png"));
+    sf::Sprite screenSprite(resourceManager.getTexture("lost_life_screen.png"));
 
     std::string backgroundImage = "background" + std::to_string(m_currentLevel) + ".png";
 
@@ -51,8 +53,8 @@ void GameManager::startGame() {
         m_gameMusic = "bossGame";
     }
     sf::Music& gamePlay = resourceManager.getMusic(m_gameMusic);
-    gamePlay.setLoop(true);
-    
+    gamePlay.setLoop(false);
+
     // m_menuMusic.setVolume(50);
     gamePlay.play();
     m_window.clear();
@@ -61,7 +63,7 @@ void GameManager::startGame() {
     bool isWindowFocused = true;
     m_board.startTimer();
 
-  
+
     while (m_window.isOpen()) {
         sf::Event event;
         while (m_window.pollEvent(event)) {
@@ -86,31 +88,109 @@ void GameManager::startGame() {
         //m_board.loadPresent(levelFile);
 
         sf::Time deltaTime = clock.restart();
-        gameState = m_board.update(deltaTime.asSeconds(), m_currentLevel); // Handles pause & game logic
-        if (gameState == LOST_GAME) break;
-        else if (gameState == WON) break;
+        gameState = m_board.update(deltaTime.asSeconds()); // Handles pause & game logic
+        if (gameState == LOST_GAME) {
+            gamePlay.stop();
+            screenSprite.setTexture(resourceManager.getTexture("gameOver.png"));
+            sf::Sound& gameOverSound = resourceManager.getSound("gameOver");
+            gameOverSound.setVolume(25);
+            gameOverSound.play();
+            sf::sleep(sf::seconds(3));
+            m_window.draw(screenSprite);
+            m_window.display();
+            sf::sleep(sf::seconds(4));
+ 
+            m_board.reset();
+            newGame();
+        }
+        else if (m_currentLevel == 6 && m_board.isLevelComplete()) {
+            gamePlay.stop();
+            screenSprite.setTexture(resourceManager.getTexture("won.png"));
+            sf::Sound& wonSound = resourceManager.getSound("won");
+            wonSound.setVolume(25);
+            wonSound.play();
+            m_window.draw(screenSprite);
+            m_window.display();
+            sf::sleep(sf::seconds(13));
+
+            m_board.reset();
+            newGame();
+        }
         else if (gameState == LOST_LIFE) {
+            gamePlay.stop();
+            sf::Sound& levelCompleteSound = resourceManager.getSound("died");
+            levelCompleteSound.setVolume(25);
+            levelCompleteSound.play();
             sf::sleep(sf::seconds(1));
 
-            m_window.draw(lostLifeSprite);
+            m_window.draw(screenSprite);
             m_window.display();
 
             sf::sleep(sf::seconds(2));
-
+            gamePlay.play();
+            m_window.clear();
+            m_window.draw(m_backgroundSprite);
+            m_board.display(m_window);
+            m_window.display();
 
         }
-        m_window.clear();
-        m_window.draw(m_backgroundSprite);
-        m_board.display(m_window);
-        m_window.display();
+        else if (gameState == TIMES_UP) {
+            gamePlay.stop();
+            screenSprite.setTexture(resourceManager.getTexture("timesUp.png"));
+            sf::Sound& timesUpSound = resourceManager.getSound("timesUp");
+            timesUpSound.setVolume(25);
+            timesUpSound.play();
+            m_window.draw(screenSprite);
+            m_window.display();
+            sf::sleep(sf::seconds(4));
+            m_currentLevel--;
+            loadNextLevel();
+        }
+        if (m_board.isPaused()) {
+            // Darken the window
+            sf::RectangleShape darkOverlay(sf::Vector2f(WINDOW_WIDTH, WINDOW_HEIGHT));
+            darkOverlay.setFillColor(sf::Color(0, 0, 0, 150)); // Semi-transparent black
+
+            // Load pause screen image
+            ResourceManager& resourceManager = ResourceManager::getInstance();
+            sf::Sprite pauseSprite(resourceManager.getTexture("pauseSign.png"));
+
+            // Center the pause image
+            pauseSprite.setPosition(
+                (WINDOW_WIDTH - pauseSprite.getGlobalBounds().width) / 2,
+                (WINDOW_HEIGHT - pauseSprite.getGlobalBounds().height) / 2
+            );
+
+            // Draw pause screen
+            m_window.clear();
+            m_window.draw(m_backgroundSprite);
+            m_board.display(m_window);
+            m_window.draw(darkOverlay);
+            m_window.draw(pauseSprite);
+            m_window.display();
+        }
+        else {
+            m_window.clear();
+            m_window.draw(m_backgroundSprite);
+            m_board.display(m_window);
+            m_window.display();
+        }
         if (m_board.isLevelComplete()) {  // Check if the player completed the level
             gamePlay.stop();
+            screenSprite.setTexture(resourceManager.getTexture("levelComplete.png"));
+            sf::Sound& levelCompleteSound = resourceManager.getSound("levelComplete");
+            levelCompleteSound.setVolume(25);
+            levelCompleteSound.play();
+            m_window.draw(screenSprite);
+            m_window.display();
+            sf::sleep(sf::seconds(3));
             loadNextLevel();  // Load the next level
         }
         processGameEvents(); //can handle here keys and mouse
 
     }
 }
+
 
 
 
@@ -127,8 +207,14 @@ void GameManager::processGameEvents() {
         case sf::Event::KeyPressed:
             m_board.handleInput(event.key.code, true);
             if (event.key.code == sf::Keyboard::Escape) {
-                m_mainMenu.Run();
-                return;
+                ResourceManager& resourceManager = ResourceManager::getInstance();
+                sf::Music& gamePlay = resourceManager.getMusic(m_gameMusic);
+                gamePlay.stop();
+                m_board.reset();
+                newGame();
+            }
+            if (event.key.code == sf::Keyboard::P) {
+                m_board.togglePause();
             }
 
         case sf::Event::KeyReleased:
@@ -180,11 +266,11 @@ void GameManager::loadNextLevel() {
     std::string levelFile = "level" + std::to_string(m_currentLevel) + ".txt";
 
     // Try loading the new level file
-    if (!m_board.loadFromFile(levelFile)) {
+    if (!m_board.loadFromFile(levelFile, m_currentLevel)) {
         std::cerr << "Error: Could not load " << levelFile << ". Restarting at Level 1." << std::endl;
         m_currentLevel = 1; // Reset to first level if not found
         levelFile = "level1.txt";
-        m_board.loadFromFile(levelFile);
+        m_board.loadFromFile(levelFile, m_currentLevel);
     }
 
     std::cout << "Loaded " << levelFile << std::endl;
