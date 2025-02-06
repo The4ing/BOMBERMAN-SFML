e0df02197430d35b146e69910dedb4f5e23b2b14
The4ing <atar.orram101@gmail.com>
2025-02-06 21:12:20 +0200

    Update FreezeGuard.h

--- include/FreezeGuard.h
+++ include/FreezeGuard.h
@@ -1,13 +1,7 @@
 #include "Present.h"
 
-
 class FreezeGuard : public Present {
-
 public:
-	FreezeGuard(); // Pass by reference
-
-
-	char getSymbol() const override; // Unique symbol
-
-
+	FreezeGuard(); 
+	char getSymbol() const override; 
 };
