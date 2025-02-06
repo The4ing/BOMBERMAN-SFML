002992d6e57731717b368c47d0b64f1d266fd1ab
The4ing <atar.orram101@gmail.com>
2025-02-06 21:10:27 +0200

    Update Coin.h

--- include/Coin.h
+++ include/Coin.h
@@ -4,25 +4,24 @@
 
 class Coin : public GameObject {
 public:
-    explicit Coin();  // Constructor
+    explicit Coin();  
     char getSymbol() const override;
     void draw(sf::RenderWindow& window) const override;
-    void update(float deltaTime);  // Function to update animation
+    void update(float deltaTime); 
     sf::CircleShape getCollisionShape() const;
-
     void handleCollision(GameObject& other) override;
-    void handleCollisionWith(Robot&) override {}// Handle collision with a Robot
-    void handleCollisionWith(Wall&) override {}        // No-op for Wall
-    void handleCollisionWith(Rock&) override {}      // No-op for another Rock
-    void handleCollisionWith(Door&) override {}        // No-op for Door
-    void handleCollisionWith(Guard&) override {}       // No-op for Guard
+    void handleCollisionWith(Robot&) override {}
+    void handleCollisionWith(Wall&) override {}       
+    void handleCollisionWith(Rock&) override {}    
+    void handleCollisionWith(Door&) override {}        
+    void handleCollisionWith(Guard&) override {}       
     void handleCollisionWith(Present&) override {}
-    void handleCollisionWith(Bomb&, bool) override {}       // No-op for Bomb
+    void handleCollisionWith(Bomb&, bool isExploding) override {}      
 
 private:
-    sf::IntRect frameRect;   // Defines the current frame
-    float frameTime;         // Time per frame
-    float elapsedTime;       // Time elapsed since last frame update
-    int frameIndex;          // Current frame index
-    int totalFrames;         // Total frames in sprite sheet
+    sf::IntRect frameRect;   
+    float frameTime;       
+    float elapsedTime;       
+    int frameIndex;          
+    int totalFrames;         
 };
