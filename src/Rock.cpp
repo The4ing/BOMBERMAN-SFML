487016bbce7dc489aae73be006630525654f8ebc
The4ing <atar.orram101@gmail.com>
2025-02-06 21:33:53 +0200

    Update Rock.cpp

--- src/Rock.cpp
+++ src/Rock.cpp
@@ -3,36 +3,18 @@
 
 Rock::Rock() {
     ResourceManager& resourceManager = ResourceManager::getInstance();
-   setTexture(resourceManager.getTexture("Rock.png")); // Assign the texture to the sprite
+   setTexture(resourceManager.getTexture("Rock.png")); 
 }
 
 char Rock::getSymbol() const {
-    return '#'; // Represents the Rock in the game
+    return '#'; 
 }
 
-//sf::Vector2f Rock::getPosition() const {
-//    return m_sprite.getPosition(); // Returns the sprite's current position
-//}
-
-//void Rock::setPosition(float x, float y) {
-//    m_sprite.setPosition(x, y); // Sets the sprite's position
-//}
-//sf::FloatRect Rock::getBoundingBox() const {
-//    return m_sprite.getGlobalBounds(); // Returns the bounding box of the Rock
-//}
-//void Rock::setScale(float scaleX, float scaleY) {
-//    m_sprite.setScale(scaleX, scaleY);
-//}
 void Rock::draw(sf::RenderWindow& window) const {
    
     window.draw(getSprite());
 }
 
 void Rock::handleCollision(GameObject& other) {
-    other.handleCollisionWith(*this); // Delegate collision handling to the other object
+    other.handleCollisionWith(*this); 
 }
-
-
-
-
-
