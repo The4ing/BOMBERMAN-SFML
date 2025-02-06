0fd6fd3a16385dc17e42adb51d39eeda339e05fa
The4ing <atar.orram101@gmail.com>
2025-02-06 21:29:04 +0200

    Update Guard.cpp

--- src/Guard.cpp
+++ src/Guard.cpp
@@ -1,38 +1,14 @@
 #include "Guard.h"
 
 
-Guard::Guard() : m_previousPosition(0.f, 0.f), m_frameWidth(127.5f), m_frameHeight(163.3f),
+Guard::Guard() : m_previousPosition(0.f, 0.f), m_frameWidth(GUARD_WIDTH), m_frameHeight(GUARD_HEIGHT),
 m_animationFrame(0), m_freezeGuard(false)
 {
-	m_numGuard++;
 }
-
-int Guard::m_numGuard = 0;
-
-
-int Guard::getGuardCount()
-{
-	return m_numGuard;
-}
-
-void Guard::restartNumGuard()
-{
-	m_numGuard = 0;
-}
-
-void Guard::reduceNumGuard()
-{
-	m_numGuard--;
-}
-
-
-
 void Guard::revertPosition()
 {
 	setPosition(m_previousPosition.x, m_previousPosition.y);
 }
-
-
 
 void Guard::setStartingPosition(float newX, float newY)
 {
@@ -48,7 +24,6 @@ sf::Vector2f Guard::getStartingPosition()
 char Guard::getSymbol() const
 {
 	 return '!'; 
-	
 }
 
 void Guard::setFreezeGaurd(const bool status)
@@ -101,33 +76,20 @@ const float Guard::getFrameHight() const
 	return m_frameHeight;
 }
 
-
-
 void Guard::draw(sf::RenderWindow& window) const
 {
-	
 	window.draw(getSprite());
-	sf::CircleShape collisionShape = getCollisionShape();
-	collisionShape.setFillColor(sf::Color::Transparent);
-	collisionShape.setOutlineColor(sf::Color::Red);
-	collisionShape.setOutlineThickness(1.f);
-	window.draw(collisionShape);
-	
 }
-
 
 void Guard::handleCollision(GameObject& other)
 {
-	
 	other.handleCollisionWith(*this);
-	
 }
 
 void Guard::handleCollisionWith(Rock&)
 {
 	revertPosition();
 }
-
 
 void Guard::handleCollisionWith(Wall&)
 {
@@ -138,10 +100,3 @@ void Guard::handleCollisionWith(Door&)
 {
 	revertPosition();
 }
-
-
-
-
-
-
-
