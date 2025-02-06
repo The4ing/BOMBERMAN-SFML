c0f99b7b5e9a25156d9620570d3bd00aca476e85
The4ing <atar.orram101@gmail.com>
2025-02-06 21:35:43 +0200

    Update Wall.cpp

--- src/Wall.cpp
+++ src/Wall.cpp
@@ -3,33 +3,12 @@
 
 Wall::Wall() {
     ResourceManager& resourceManager = ResourceManager::getInstance();
-
-
     setTexture(resourceManager.getTexture("wall.png"));
 }
 
-
-//sf::Vector2f Wall::getPosition() const {
-//    return m_sprite.getPosition();
-//}
-
-
-//void Wall::setPosition(float x, float y) {
-//    m_sprite.setPosition(x, y);
-//}
-//sf::FloatRect Wall::getBoundingBox() const {
-//    return m_sprite.getGlobalBounds();
-//}
-
-//void Wall::setScale(float scaleX, float scaleY) {
-//    m_sprite.setScale(scaleX, scaleY);
-//}
 char Wall::getSymbol() const {
     return '#';
 }
-
-
-
 
 void Wall::draw(sf::RenderWindow& window) const {
     
