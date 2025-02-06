44cc6a5692a296577e56cfe1b75a9d05abf98013
The4ing <atar.orram101@gmail.com>
2025-02-06 21:18:48 +0200

    Update StupidGuard.h

--- include/StupidGuard.h
+++ include/StupidGuard.h
@@ -7,39 +7,15 @@
 class StupidGuard : public Guard {
 public:
     StupidGuard();
-
-    // Overridden methods
-    
-   
-    
     void update(float deltaTime) override;
-
-    // Specific collision handling
-    
- //   void handleCollisionWith(Wall&) override;
     void handleCollisionWith(Guard&) override {}
-    void handleCollisionWith(Bomb&, bool ) override;        // No-op for Bomb
-
-    
+    void handleCollisionWith(Bomb&, bool isExploding) override;  
     sf::CircleShape getCollisionShape() const override;
 
-    
 private:
-    // Helper Methods
     void changeDirection();
-   
-    
-  
-
-    sf::Clock m_animationClock;     // Clock for animating the guard
-    sf::Clock m_movementClock;      // Clock for changing direction
-    sf::Time m_randomChangeInterval; // Interval to change direction
+    sf::Time m_randomChangeInterval; 
     sf::Time m_timeSinceLastChange;
     sf::Time m_animationTimeSinceLastChange;
-   // sf::Vector2f m_startingPosition;
-    
-    // Movement-related members
-   
-    
-    Direction m_direction;          // Current movement direction
+    Direction m_direction;        
 };
